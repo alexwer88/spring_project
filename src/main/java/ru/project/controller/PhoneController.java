e0b85b80c3407bd3434f8ff9c42bb1801b3833a7
aw <a.verizhnikov@boloid.com>
2017-04-08 21:10:59 +0300

    Удаление лишних файлов

--- src/main/java/ru/project/controller/PhoneController.java
+++ src/main/java/ru/project/controller/PhoneController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.project.model.protocol.PhoneProtocol;
-import ru.project.service.PhoneService;
+
 import javax.validation.constraints.NotNull;
 
 /**
